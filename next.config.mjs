7ebc8bbbf7e990e2b3f3e67ffbc239d56403beed
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-03-15 10:48:22 +0800

    upd: 调整配置文件

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,16 @@
  * @Author: Chen HuaKang
  * @Date: 2024-03-15 10:08:57
  * @LastEditors: Chen HuaKang
- * @LastEditTime: 2024-03-15 10:39:54
+ * @LastEditTime: 2024-03-15 10:46:45
  * @Description: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    loader: 'custom',
-    loaderFile: '',
-  },
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './page.js',
+  // },
 };
 
 export default nextConfig;
