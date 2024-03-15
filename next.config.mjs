90f09fd966b8cd99b2b3be6e450b77693997feaa
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-03-15 11:16:28 +0800

    upd: 调整自动打包错误问题

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
  * @Author: Chen HuaKang
  * @Date: 2024-03-15 10:08:57
  * @LastEditors: Chen HuaKang
- * @LastEditTime: 2024-03-15 11:07:56
+ * @LastEditTime: 2024-03-15 11:15:34
  * @Description: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   // images: {
   //   loader: 'custom',
   // },
