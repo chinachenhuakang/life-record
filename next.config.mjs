5808d1f7df6ef3dfd4508adeb5628b18ae08b1c8
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-03-15 14:03:40 +0800

    格式问题

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,12 @@
  * @Author: Chen HuaKang
  * @Date: 2024-03-15 10:08:57
  * @LastEditors: Chen HuaKang
- * @LastEditTime: 2024-03-15 11:15:34
+ * @LastEditTime: 2024-03-15 14:02:46
  * @Description: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // images: {
-  //   loader: 'custom',
-  // },
 };
 
 export default nextConfig;
