aafc0ae101ab7fd94cca47de5499c7ba94918d80
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-03-15 11:01:49 +0800

    upd: 按报错信息填内容

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
  * @Author: Chen HuaKang
  * @Date: 2024-03-15 10:08:57
  * @LastEditors: Chen HuaKang
- * @LastEditTime: 2024-03-15 10:46:45
+ * @LastEditTime: 2024-03-15 11:01:28
  * @Description: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'output: export',
   // images: {
   //   loader: 'custom',
   //   loaderFile: './page.js',
