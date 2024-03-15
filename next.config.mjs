db253612521770b395d584cb36a7c0bbf83f49b0
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-03-15 10:27:59 +0800

    upd: 调整next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+/*
+ * @Author: Chen HuaKang
+ * @Date: 2024-03-15 10:08:57
+ * @LastEditors: Chen HuaKang
+ * @LastEditTime: 2024-03-15 10:27:21
+ * @Description: 
+ */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './page.js',
+  },
+};
 
 export default nextConfig;
