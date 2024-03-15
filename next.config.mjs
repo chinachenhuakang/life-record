e3a1a24ffc271fa8e81642f1d196608b44371bdd
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-03-15 17:37:59 +0800

    upd: 调整next 发布

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,17 @@
  * @Author: Chen HuaKang
  * @Date: 2024-03-15 10:08:57
  * @LastEditors: Chen HuaKang
- * @LastEditTime: 2024-03-15 14:02:46
+ * @LastEditTime: 2024-03-15 17:35:33
  * @Description: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images:{
+    unoptimized: true,
+  },
+  distDir: 'out',
 };
 
+// module.exports = nextConfig;
 export default nextConfig;
