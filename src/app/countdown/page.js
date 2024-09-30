724314a7072d82b0681a76708eed3db92c2143d4
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-09-30 16:46:26 +0800

    upd: 调整倒数日

--- src/app/countdown/page.js
+++ src/app/countdown/page.js
@@ -10,10 +10,23 @@ import menuCategories from '@/data/menu';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { Tab } from '@headlessui/react';
+import moment from 'moment';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
+
+const ONE_DAY = 86400000;
+const nowDate = new Date();
+
+const LOVE_DAY = {
+  MEETING_DAY: '2024-2-4',
+};
+const HOLIDAY_MAP = {
+  NEW_YEAR_DAY: '元旦',
+  MAY_DAY: '五一',
+  NATIONAL_DAY: '十一',
+};
 
 export default function Countdown() {
   const [countDay, setCountDay] = useState([]);
@@ -55,42 +68,75 @@ export default function Countdown() {
       },
     ]);
   };
+
+  const tranCNTimeStatus = (diff) => {
+    return diff >= 0 ? '还有' : '过去';
+  };
+
+  const handelDistanceDay = (diffDay) => {
+    return diffDay === 0 ? 1 : Math.abs(diffDay);
+  };
+
+  const handleCombinationText = (modleDay, year = moment(nowDate).format('YYYY')) => {
+    let diffDistanceDay = 0;
+    const yearString = year ? moment(year).format('YYYY') : moment(nowDate).format('YYYY');
+    switch (modleDay) {
+      case HOLIDAY_MAP.NEW_YEAR_DAY:
+        diffDistanceDay = moment(`${yearString}-1-1`).diff(moment(nowDate), 'day');
+        break;
+      case HOLIDAY_MAP.MAY_DAY:
+        diffDistanceDay = moment(`${yearString}-5-1`).diff(moment(nowDate), 'day');
+        break;
+      case HOLIDAY_MAP.NATIONAL_DAY:
+        diffDistanceDay = moment(`${yearString}-10-1`).diff(moment(nowDate), 'day');
+        break;
+      default:
+        return `错误节假日`;
+    }
+    return `${tranCNTimeStatus(diffDistanceDay)} ${handelDistanceDay(diffDistanceDay)} 天`;
+  };
+
   useEffect(() => {
     // initData();
-    let oneDay = 86400000;
-    let newTime = new Date().getTime(),
-      meetDay = (newTime - new Date('2024-2-4').getTime()) / oneDay,
-      yiyiDay = Math.ceil(
-        (newTime - new Date(`${new Date().getFullYear()}-1-1`).getTime()) / oneDay
-      ),
-      wuyiDay = Math.ceil(
-        (newTime - new Date(`${new Date().getFullYear()}-5-1`).getTime()) / oneDay
-      ),
-      shiyiDay = Math.ceil(
-        (newTime - new Date(`${new Date().getFullYear()}-10-1`).getTime()) / oneDay
-      );
+    let nextYear = moment(nowDate).add(1, 'year'),
+      meetDay = Math.abs(moment(LOVE_DAY.MEETING_DAY).diff(moment(nowDate), 'day')) + 1,
+      nextYearString = nextYear.format('YYYY');
     setCountDay([
-      { id: 1, name: '在一起', value: `第 ${Math.ceil(meetDay)} 天` },
+      { id: 1, name: '在一起', value: `第 ${meetDay} 天` },
       {
         id: 2,
         name: '距离元旦',
-        value: `${yiyiDay < 0 ? '还有' : '过去'} ${
-          yiyiDay < 0 ? Math.abs(yiyiDay) + 1 : Math.abs(yiyiDay) - 1
-        } 天`,
+        value: `${handleCombinationText(HOLIDAY_MAP.NEW_YEAR_DAY)}`,
       },
       {
         id: 3,
         name: '距离五一',
-        value: `${wuyiDay < 0 ? '还有' : '过去'} ${
-          wuyiDay < 0 ? Math.abs(wuyiDay) + 1 : Math.abs(wuyiDay) - 1
-        } 天`,
+        value: `${handleCombinationText(HOLIDAY_MAP.MAY_DAY)}`,
       },
       {
         id: 4,
         name: '距离十一',
-        value: `${shiyiDay < 0 ? '还有' : '过去'} ${
-          shiyiDay < 0 ? Math.abs(shiyiDay) + 1 : Math.abs(shiyiDay) - 1
-        } 天`,
+        value: `${handleCombinationText(HOLIDAY_MAP.NATIONAL_DAY)}`,
+      },
+      {
+        id: 5,
+        name: '',
+        value: `${nextYearString}`,
+      },
+      {
+        id: 6,
+        name: '距离元旦',
+        value: `${handleCombinationText(HOLIDAY_MAP.NEW_YEAR_DAY, nextYearString)}`,
+      },
+      {
+        id: 7,
+        name: '距离五一',
+        value: `${handleCombinationText(HOLIDAY_MAP.MAY_DAY, nextYearString)}`,
+      },
+      {
+        id: 8,
+        name: '距离十一',
+        value: `${handleCombinationText(HOLIDAY_MAP.NATIONAL_DAY, nextYearString)}`,
       },
     ]);
     return () => {
