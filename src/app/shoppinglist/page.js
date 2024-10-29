55cd2258ba990a281c6fdb68b642e8e964274aad
HuaKang Chen <chenhuakang050@chinasoftinc.com>
2024-10-29 11:54:31 +0800

    upd: 加入确定增加物品，已购和未购的区别

--- src/app/shoppinglist/page.js
+++ src/app/shoppinglist/page.js
@@ -10,7 +10,6 @@ const ShoppingList = () => {
   }
   const [textProdcut, setTextProdcut] = useState('');
   const [productList, setProductList] = useState(storeProductList || []);
-  const inputRef = useRef(null);
 
   const handleAddProduct = () => {
     if (!textProdcut) {
@@ -62,6 +61,13 @@ const ShoppingList = () => {
     }
   };
 
+  const handleInputKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === 'NumpadEnter') {
+      // 调用你想要执行的函数
+      handleAddProduct();
+    }
+  };
+
   const total = useMemo(() => productList?.length, [productList]);
   const doneCount = useMemo(() => productList?.filter((item) => item.done)?.length, [productList]);
 
@@ -79,12 +85,13 @@ const ShoppingList = () => {
             className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
             value={textProdcut}
             onChange={handleChangeText}
+            onKeyDown={handleInputKeyDown}
           ></input>
           <button
             className='  ml-auto block w-16 rounded-md bg-green-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600'
             onClick={handleAddProduct}
           >
-            加菜
+            加购
           </button>
           {/* <button
             className='block w-16 rounded-md bg-green-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600'
@@ -100,8 +107,8 @@ const ShoppingList = () => {
           {productList.map((product, index) => (
             <li
               key={index}
-              className={`h-24 p-2 bottom-1 border-dashed mt-4 text-gray-50  ${
-                product.done ? 'bg-green-700' : 'bg-red-300'
+              className={`h-24 p-2 bottom-1 border-dashed mt-4 text-gray-50 relative ${
+                product.done ? 'bg-green-700' : 'bg-gray-500'
               }`}
               style={{
                 display: 'flex',
@@ -111,11 +118,20 @@ const ShoppingList = () => {
               }}
               onClick={() => handleShoppingProductDone(product)}
             >
+              {product.done ? (
+                <>
+                  <span className=' absolute top-1 left-0 -rotate-45'>已购</span>
+                </>
+              ) : (
+                <>
+                  <span className=' absolute top-1 left-0 -rotate-45'>未购</span>
+                </>
+              )}
               <label htmlFor='message' className='block text-sm font-semibold leading-6'>
                 {product.name}
               </label>
               <button
-                className='block w-16 rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                className='block w-16 rounded-md bg-red-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400'
                 style={{ marginLeft: 'auto' }}
                 onClick={() => handleDeleteProduct(index)}
               >
